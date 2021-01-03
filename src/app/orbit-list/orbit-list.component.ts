4ce395ba205316a10c0373eb69f91ca5a050a3cf
SPoon <poonselinasl@gmail.com>
2021-01-02 22:39:52 -0600

    display table of satellites

--- src/app/orbit-list/orbit-list.component.ts
+++ src/app/orbit-list/orbit-list.component.ts
@@ -7,8 +7,10 @@ import { Satellite } from '../satellite';
   templateUrl: './orbit-list.component.html',
   styleUrls: ['./orbit-list.component.css']
 })
-export class OrbitListComponent implements OnInit {
 
+
+
+export class OrbitListComponent implements OnInit {
   @Input() satellites: Satellite[];
 
   constructor() { }
